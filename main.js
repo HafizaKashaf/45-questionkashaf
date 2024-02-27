71a21a16b37e4bfca157f437401f4b3f69fe0cdb
HafizaKashaf <hafizakashafansari@gmail.com>
2024-02-27 11:02:57 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,5 @@
+"use strict";
+let massage = "kashaf!";
+console.log(massage);
+massage = "1234";
+console.log(massage);
---
+++ main.ts
@@ -0,0 +1,4 @@
+let massage :string = "kashaf!";
+console.log(massage);
+massage = "1234";
+console.log(massage);
